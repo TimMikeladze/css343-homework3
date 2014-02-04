bebcc93d3a424b51aa38c19fcce37d8412336d83
TimMikeladze <tim.mikeladze@gmail.com>
2014-02-04 12:26:33 -0800

    starter code

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+import javax.swing.tree.TreeNode;
+
+public class Test {
+	
+	public static int inversion(int[] inputs) {
+		return 0;
+		//add your code for Q1 here
+	}
+	
+	public static boolean isBinarySearchTree(TreeNode root) {
+		return false;
+		//add your code for Q2 here.
+		
+	}
+	
+	public static int maxSum(TreeNode root) {
+		return 0;
+		//add your code for Q3 here.
+		
+	}
+	
+	public static void main(String args[]) {
+		//I will take care of the test part. 		
+	}
+	
+}
