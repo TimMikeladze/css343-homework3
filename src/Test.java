40b506517f56c32a66e7765d0876d844e8bc30cb
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-02-04 15:28:36 -0800

    question 2 code

--- src/Test.java
+++ src/Test.java
@@ -18,18 +18,42 @@ public class Test {
     }
 
     public static int maxSum(TreeNode root) {
-        return 0;
-        //add your code for Q3 here.
+        return maxSumHelper(root)[1];
+    }
 
+    private static int[] maxSumHelper(TreeNode root) {
+        int values[] = { 0, 0 };
+        if (root != null) {
+            int l[] = maxSumHelper(root.left);
+            int r[] = maxSumHelper(root.right);
+
+            values[0] = Math.max(0, Math.max(l[0], r[0]) + root.data);
+            values[1] = Math.max(Math.max(l[1], r[1]), l[0] + root.data + r[0]);
+        }
+        return values;
     }
 
     public static void main(String args[]) {
+        /*
         TreeNode r = new TreeNode(4);
         r.left = new TreeNode(2);
         r.left.left = new TreeNode(1);
         r.left.right = new TreeNode(3);
         r.right = new TreeNode(5);
         System.out.println(isBinarySearchTree(r));
+        */
+
+        /*
+        TreeNode r = new TreeNode(-10);
+        r.left = new TreeNode(2);
+        r.left.left = new TreeNode(4);
+        r.left.right = new TreeNode(5);
+        r.right = new TreeNode(3);
+        r.right.right = new TreeNode(9);
+        r.right.left = new TreeNode(7);
+        System.out.println(maxSum(r));
+        */
+
     }
 
 }
