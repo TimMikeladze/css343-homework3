9f690862b3c1699fff81793ec7501d19c85d496d
TimMikeladze <tim.mikeladze@gmail.com>
2014-02-05 03:23:46 -0800

    finished homework

--- src/Test.java
+++ src/Test.java
@@ -1,59 +1,81 @@
 public class Test {
-
-    public static int inversion(int[] inputs) {
-        return 0;
-        //add your code for Q1 here
-    }
-
-    public static boolean isBinarySearchTree(TreeNode root) {
-        return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-
-    }
-
-    private static boolean isBinarySearchTree(TreeNode root, int min, int max) {
-        return root == null
-                || (root.data > min && root.data < max && isBinarySearchTree(root.left, min, root.data) && isBinarySearchTree(
-                        root.right, root.data, max)) ? true : false;
-
-    }
-
-    public static int maxSum(TreeNode root) {
-        return maxSumHelper(root)[1];
-    }
-
-    private static int[] maxSumHelper(TreeNode root) {
-        int values[] = { 0, 0 };
-        if (root != null) {
-            int l[] = maxSumHelper(root.left);
-            int r[] = maxSumHelper(root.right);
-
-            values[0] = Math.max(0, Math.max(l[0], r[0]) + root.data);
-            values[1] = Math.max(Math.max(l[1], r[1]), l[0] + root.data + r[0]);
-        }
-        return values;
-    }
-
-    public static void main(String args[]) {
-        /*
-        TreeNode r = new TreeNode(4);
-        r.left = new TreeNode(2);
-        r.left.left = new TreeNode(1);
-        r.left.right = new TreeNode(3);
-        r.right = new TreeNode(5);
-        System.out.println(isBinarySearchTree(r));
-        */
-
-        /*
-        TreeNode r = new TreeNode(-10);
-        r.left = new TreeNode(2);
-        r.left.left = new TreeNode(4);
-        r.left.right = new TreeNode(5);
-        r.right = new TreeNode(3);
-        r.right.right = new TreeNode(9);
-        r.right.left = new TreeNode(7);
-        System.out.println(maxSum(r));
-        */
-
-    }
-
+	
+	public static int inversion(int[] inputs) {
+		return inversion(inputs, 0, inputs.length);
+	}
+	
+	private static int inversion(int[] inputs, int left, int right) {
+		if (right > inputs.length || left < 0 || left >= right) {
+			return 0;
+		}
+		
+		int partition = left + (right - left) / 2;
+		
+		int inversions = 0;
+		
+		if (partition > left + 1) {
+			inversions += inversion(inputs, left, partition);
+		}
+		if (partition < right - 1) {
+			inversions += inversion(inputs, partition, right);
+		}
+		
+		inversions += merge(inputs, left, partition, right);
+		return inversions;
+	}
+	
+	private static int merge(int[] inputs, int left, int partition, int right) {
+		int temp[] = new int[inputs.length];
+		System.arraycopy(inputs, left, temp, left, right - left);
+		
+		int inversions = 0;
+		
+		int a = left;
+		int b = partition;
+		for (int i = left; i < right; i++) {
+			if (a >= partition || (b < right && temp[b] < temp[a])) {
+				inversions += (b - i);
+				inputs[i] = temp[b++];
+			}
+			else {
+				inputs[i] = temp[a++];
+			}
+		}
+		
+		return inversions;
+	}
+	
+	public static boolean isBinarySearchTree(TreeNode root) {
+		return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		
+	}
+	
+	private static boolean isBinarySearchTree(TreeNode root, int min, int max) {
+		return root == null
+				|| (root.data > min && root.data < max && isBinarySearchTree(root.left, min, root.data) && isBinarySearchTree(root.right, root.data,
+						max)) ? true : false;
+		
+	}
+	
+	public static int maxSum(TreeNode root) {
+		return maxSumHelper(root)[1];
+		
+	}
+	
+	private static int[] maxSumHelper(TreeNode root) {
+		int sums[] = { 0, 0 };
+		if (root != null) {
+			int left[] = maxSumHelper(root.left);
+			int right[] = maxSumHelper(root.right);
+			
+			sums[0] = Math.max(0, Math.max(left[0], right[0]) + root.data);
+			sums[1] = Math.max(Math.max(left[1], right[1]), left[0] + root.data + right[0]);
+		}
+		return sums;
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+	
 }
